dafd6c2cd9305cd1e9e808c4f5ce50d264c195aa
Nitish Kumar <nitish441999@gmail.com>
2024-11-29 10:47:56 +0530

    some change

--- index.js
+++ index.js
@@ -8,29 +8,25 @@ const app = express();
 
 // Connect to the database
 connectDb();
-//Middleware
-app.use(cors()); // Enable CORS for cross-origin requests
-app.use(express.json()); // Enable JSON body parsing
 
-//// Routes
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.use("/api/v1/auth", require("./Router/AddEmployeeRouter"));
-app.use("/api/v1/auth", require("./Router/AddEmployeeRouter"));
 app.use("/api/v1/employee", require("./Router/UserRouter"));
-app.use("/api/v1/employee", require("./Router/UserRouter"));
-app.use("/api/v1/employee", require("./Router/UserRouter"));
-app.use("/api/v1/employee", require("./Router/UserRouter"));
-app.use("/api/v1/department", require("./Router/DepartmentRouter"));
-app.use("/api/v1/department", require("./Router/DepartmentRouter"));
-app.use("/api/v1/department", require("./Router/DepartmentRouter"));
 app.use("/api/v1/department", require("./Router/DepartmentRouter"));
 
+// Root route
 app.get("/", (req, resp) => {
   return resp.status(200).send("<h1>Hi Server Start</h1>");
 });
-
-// Set the port and start the server
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Export for Vercel
+module.exports = app;

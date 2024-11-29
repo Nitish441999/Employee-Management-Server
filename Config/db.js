d3c3a5c62fe221551963b32350516c415bd877d3
Nitish Kumar <nitish441999@gmail.com>
2024-11-29 11:53:45 +0530

    some change

--- Config/db.js
+++ Config/db.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
-
-// Connection to MongoDB
+const mongoose = require('mongoose')
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to the database");
+      await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("MongoDB Connected...");
     } catch (error) {
-        console.error('DB Connection Error:');
-        process.exit(1); // Exit process with failure
+      console.error("Database connection error:", error.message);
+      process.exit(1); // Exit process with failure
     }
-};
-
-module.exports = connectDb;
+  };
+  
+  module.exports = connectDb;
+  

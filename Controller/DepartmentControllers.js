0a3903e30b33952ee6fd76a3ab97ffab3950ef29
Nitish Kumar <nitish441999@gmail.com>
2024-11-30 09:52:41 +0530

    some change

--- Controller/DepartmentControllers.js
+++ Controller/DepartmentControllers.js
@@ -8,7 +8,7 @@ const departmentController = async (req, resp) => {
     if (department) {
       return resp.status(404).json({
         success: false,
-        message: "Department already exists. Please login.",
+        message: "Department already exists.",
       });
     }
     // create new department
@@ -28,82 +28,80 @@ const departmentController = async (req, resp) => {
 };
 
 const getDepartmentController = async (req, resp) => {
-    try {
-        const department = await DepartmentModel.find({})
-        if (!department.length) {
-            return resp.status(404).json({
-                success: false,
-                message: "Department List Not Found"
-            });
-        }
-
-        resp.status(200).json({
-            success: true,
-            message: department.length,
-            department
-        });
-
-    } catch (error) {
-        console.log(error);
-        resp.status(500).json({
-            success: false,
-            message: "Error in Get Department API",
-            error: error.message || error
-        });
+  try {
+    const department = await DepartmentModel.find({});
+    if (!department.length) {
+      return resp.status(404).json({
+        success: false,
+        message: "Department List Not Found",
+      });
     }
+
+    resp.status(201).json({
+      success: true,
+      message: department.length,
+      department,
+    });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      success: false,
+      message: "Error in Get Department API",
+      error: error.message || error,
+    });
+  }
 };
 
-const deleteDepartmentController= async(req, resp) =>{
-    try {
-        const department = await DepartmentModel.findByIdAndDelete(req.params.id)
+const deleteDepartmentController = async (req, resp) => {
+  try {
+    const department = await DepartmentModel.findByIdAndDelete(req.params.id);
 
-        resp.status(201).json({
-            success: true,
-            message: "Department Has Been Deleted Successfully",
-          });
-        
-    } catch (error) {
-        console.log(error);
-        resp.status(500).json({
-            success:false,
-            message: "Error in Delete Department Api",
-            error: error.message ||error
-        })
-        
-        
-    }
-
-}
+    resp.status(201).json({
+      success: true,
+      message: "Department Has Been Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      success: false,
+      message: "Error in Delete Department Api",
+      error: error.message || error,
+    });
+  }
+};
 
 const updateDepartment = async (req, resp) => {
-    const { id } =req.params
-    try {
-        const department = await DepartmentModel.findByIdAndUpdate(id) 
-        if(!department){
-            resp.status(404).json({
-                success:false,
-                message: "Department is not Fund"
-            })
-        }
-        const { departmentName } = req.body
-        if(departmentName) department.departmentName = departmentName
-
-        await department.save()
-        resp.status(201).json({
-            success: true,
-            massage: "Department Update Successfully",
-            department
-        })
-    } catch (error) {
-        console.log(error)
-        resp.status(500).json({
-            success: false,
-            message: "Error in Update API",
-            error: error.message||error
-        })
-        
+  const { id } = req.params;
+  try {
+    const department = await DepartmentModel.findByIdAndUpdate(id);
+    if (!department) {
+      resp.status(404).json({
+        success: false,
+        message: "Department is not Fund",
+      });
     }
-    
-}
+    const { departmentName } = req.body;
+    if (departmentName) department.departmentName = departmentName;
 
-module.exports = { departmentController, getDepartmentController, deleteDepartmentController, updateDepartment };
+    await department.save();
+    resp.status(201).json({
+      success: true,
+      massage: "Department Update Successfully",
+      department,
+    });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      success: false,
+      message: "Error in Update API",
+      error: error.message || error,
+    });
+  }
+};
+
+module.exports = {
+  departmentController,
+  getDepartmentController,
+  deleteDepartmentController,
+  updateDepartment,
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const connectDb = require("./Config/db");
+const connectDb = require("../Config/db");
 require("dotenv").config();
 
 // Initialize express app
@@ -14,9 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/v1/auth", require("./Router/AddEmployeeRouter"));
-app.use("/api/v1/employee", require("./Router/UserRouter"));
-app.use("/api/v1/department", require("./Router/DepartmentRouter"));
+app.use("/api/v1/auth", require("../Router/AddEmployeeRouter"));
+app.use("/api/v1/employee", require("../Router/UserRouter"));
+app.use("/api/v1/department", require("../Router/DepartmentRouter"));
 
 // Root route
 app.get("/", (req, resp) => {
